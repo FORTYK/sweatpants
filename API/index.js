2740d67d0dcdfd0b34adeddd92fb4a44408b7d95
Christian <christian.augustsson@gmail.com>
2021-05-29 14:03:20 +0200

    route order documentation

--- API/index.js
+++ API/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Routes that requires no auth
 // Such as performing authentication
 router.use("/handshake", require("./routes/handshake"));
+
+// Requires CSRF
 router.use("/session", require("./routes/session"));
 
-// Requires logged in?
+// Requires User to be logged in?
 router.use("/user", require("./routes/user"));
 router.use("/post", require("./routes/post"));
 
