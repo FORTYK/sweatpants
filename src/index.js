82dbacf54d6f97348932174119ffaaeb45350456
Christian <christian.augustsson@gmail.com>
2021-05-29 14:04:30 +0200

    Fix user cookie
    Cookie provider wrapper in react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CookiesProvider } from 'react-cookie';
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
