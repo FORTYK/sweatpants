683d9b77cbd599209132bf37c910873d9efd0f20
Christian <christian.augustsson@gmail.com>
2021-05-29 13:47:34 +0200

    Setting up lib
    Seperates validation and sanitization from functionality

---
+++ API/lib/session.js
@@ -0,0 +1,60 @@
+const { err } = require('./../tools.js');
+
+var session = {
+  async initialize(req, res) {
+    try {
+      await req.session.regenerate(async () => {
+        req.session.isSet = true;
+        req.session.guest = true;
+        await req.session.save();
+
+        res.send({ user: {} });
+      });
+    } catch (error) {
+      // log error
+      res.send(err('Session could not initialize'))
+    }
+  },
+
+  async logout(req, res) {
+    let isLogout = await this.initialize(req, res);
+
+    if (isLogout) {
+      return res.send({ user: {} });
+    } else {
+      return res.status(500).send(err(""));
+    }
+  },
+
+
+  async login(req, res, user) {
+    console.log('user :>> ', user);
+    try {
+      await req.session.regenerate(async () => {
+        req.session.isSet = true;
+        req.session.guest = false;
+        req.session.userid = user.iduser;
+        req.session.login = user.login;
+        req.session.email = user.email;
+        req.session.display = user.display;
+
+        await req.session.save();
+        res.send({
+          user: {
+            iduser: req.session.iduser,
+            login: req.session.login,
+            email: req.session.email,
+            display: req.session.display
+          }
+        });
+      });
+    } catch (error) {
+      console.log("login error :>> ", error);
+      // log error
+
+      res.status(500).send(err(""));
+    }
+  }
+}
+
+module.exports = session;
---
+++ API/lib/user.js
@@ -0,0 +1,10 @@
+var user = {
+  create(req, res) {
+    return res.send({ title: "response" });
+  },
+  update(req, res) {
+    return res.send({ title: "response" });
+  },
+}
+
+module.exports = user;
